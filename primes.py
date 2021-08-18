c43c94fa503c7aeac6502affd892cac6070f02b2
Lori White <lori.r.white.24@gmail.com>
2021-08-18 14:34:31 -0400

    Testsed the script to verify that it produced the expected results in the results.txt file.

--- primes.py
+++ primes.py
@@ -1,7 +1,9 @@
 # Author: Lori White
 # This program prints the primes from a to b, which are specified by the user.
 # a and b must be an integer and a < b.
+from io import FileIO
 from math import sqrt
+import re
 
 # Checks if the number is prime or not
 def isPrime(num):
@@ -56,12 +58,12 @@ def checkValue(a, b):
     assert (a < b), f"The ending value cannot be equal to or less than the starting value, {a}!"
 
 # Gets the user's choice for exiting the program 
-def getOption():
+def getOption(choice):
     option = ""
     contain = False
     while not contain :
         try:
-            option = input("\nDo you wish to exit this program? (Y/N)  ").upper()
+            option = input(f"\nDo you wish to {choice}? (Y/N)  ").upper()
             contain = checkOption(option)
         except AssertionError as e:
             print(f"{e} Please, try again.")
@@ -75,6 +77,23 @@ def checkOption(option):
     assert ("Y" in option or "N" in option), "You must enter Y or N!"
     return True
 
+# Saves results to a file if the user wishes to
+def savePrimes(primes):
+    save = getOption("save results to a file")
+    p = re.compile("[<>:\/\\|?*]")
+    if save:
+        saveFile = None
+        while True:
+            try:
+                name = input("\nWhat name do you want to give to the results file? (the file will be saved as a text file) ") + ".txt"
+                assert (p.match(name) == None), "Invalid file name!"
+                saveFile = open(name, "w")
+            except (AssertionError, OSError) as e:
+               print(f"\n{e} Please try again.")
+            else: break
+        saveFile.write(primes)
+        saveFile.close()
+
 # Runs the Prime Checker program
 def primeChecker():
     print("Welcome to Prime Checker!\nHere you can list all the primes for a specified range of values.")
@@ -83,8 +102,10 @@ def primeChecker():
         first = getA()
         last = getB(first)
         primes = getPrimes(findPrimes(first, last))
-        print(f"\nThe primes within the range of {first} and {last} are {primes}.")
-        exit = getOption()
+        output = f"\nThe primes within the range of {first} and {last} are {primes}."
+        print(output)
+        savePrimes(output.strip())
+        exit = getOption("exit this program")
     print("\nGoodbye!")
 
 # Main Program
